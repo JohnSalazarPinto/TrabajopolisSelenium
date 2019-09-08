66f1516b99bd715b95d130870bea4e3506c4b30f
John Salazar Pinto <johnpiterzon@gmail.com>
2019-09-08 00:52:41 -0400

    migration project from browser branch unnecessary files deleted

---
+++ src/main/java/core/selenium/WebDriverManager.java
@@ -0,0 +1,57 @@
+/*
+ * @(#) WebDriverManager.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av. Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package core.selenium;
+
+import core.selenium.webdriver.ConnectionFactory;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * WebDriverManager class.
+ *
+ * @author John Salazar Pinto
+ * @version 1.0
+ */
+public class WebDriverManager {
+    public WebDriverConfig webDriverConfig;
+    private static WebDriver driver;
+    private static WebDriverManager webDriverManager;
+
+    /**
+     * Constructor sets properties.
+     */
+    private WebDriverManager() {
+        ConnectionFactory connection = new ConnectionFactory();
+        driver = connection.selector(webDriverConfig.getInstance().getBrowser());
+    }
+
+    /**
+     * Gets instance for init properties.
+     *
+     * @return webDriverManager.
+     */
+    public static WebDriverManager getInstance() {
+        if (webDriverManager == null) {
+            webDriverManager = new WebDriverManager();
+        }
+        return webDriverManager;
+    }
+
+    /**
+     * Gets driver for common methods.
+     *
+     * @return driver
+     */
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
---
+++ src/main/java/core/selenium/webdriver/Chrome.java
@@ -0,0 +1,51 @@
+/*
+ * @(#) Chrome.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av. Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+package core.selenium.webdriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+/**
+ * Chrome class.
+ *
+ * @author John Salazar Pinto
+ * @version 1.0
+ */
+public class Chrome implements IBrowser {
+
+    private static WebDriver driver;
+
+    /**
+     * Constructor class init the conecction.
+     */
+    public void connection() {
+        driverConnection();
+    }
+
+    /**
+     * Method for connect with chrome.
+     *
+     * @return driver - WebDriver.
+     */
+    public static WebDriver driverConnection() {
+        ChromeDriverManager.getInstance().version("76.0.3809.126").setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.amazon.com//");
+        driver.manage().window().maximize();
+        return driver;
+    }
+}
