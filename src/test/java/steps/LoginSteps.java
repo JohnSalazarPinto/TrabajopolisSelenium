aee9fe326aebcb05caf35a040937d0d33d3b9c19
John Salazar Pinto <johnpiterzon@gmail.com>
2019-09-08 00:16:17 -0400

    credential deleted assert testNG implemented

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -15,6 +15,7 @@ package steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import project.ui.pages.LoginPage;
 
@@ -48,6 +49,6 @@ public class LoginSteps {
      */
     @Then("user login succefuly")
     public void userLoginSuccefuly() {
-
+        Assert.assertEquals(loginPage.message(),"Authentication required");
     }
 }
