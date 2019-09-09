ede34a33b5c36f170f20556c21a4de9191cd837f
John Salazar Pinto <johnpiterzon@gmail.com>
2019-09-09 11:04:34 -0400

    cart feature added

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -17,7 +17,6 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import project.ui.pages.LoginPage;
 
 /**
