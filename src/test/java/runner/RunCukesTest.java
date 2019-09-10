0d2f00712b946fb0eca4c21df53cfa36a504002f
John Salazar Pinto <johnpiterzon@gmail.com>
2019-09-10 10:37:16 -0400

    runner and report implemented

---
+++ src/test/java/runner/RunCukesTest.java
@@ -0,0 +1,42 @@
+package runner;/*
+ * @(#) RunCukesTest.java Copyright (c) 2019 Jala Foundation.
+ * 2643 Av. Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Jala Foundation, ("Confidential Information").  You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Jala Foundation.
+ */
+
+
+import core.report.GeneratorReport;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterTest;
+
+/**
+ * RunCukesTest class.
+ *
+ * @author Cristian Lujan
+ * @version 1.0
+ */
+@CucumberOptions(
+        plugin = {"pretty",
+                "html:target/cucumber-pretty",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"},
+        glue = {"com/foundation/salesforce/steps", "com/foundation/salesforce/hooks"},
+        features = {"src/test/resources/features/"},
+        monochrome = true)
+public class RunCukesTest extends AbstractTestNGCucumberTests {
+
+    /**
+     * The method afterExecution executes the project and generate it report.
+     */
+    @AfterTest
+    public void afterExecution() {
+        GeneratorReport.getInstance().generateReport();
+    }
+}
